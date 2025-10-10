f29b70305285edbbb0023741cad7c8be23b3a37b
jvziin10 <joao.franca.santos22@escola.pr.gov.br>
2025-10-10 08:04:52 -0300

    ..

---
+++ perguntas.js
@@ -0,0 +1,11 @@
+criaCartao('Programação',
+'O que é python?',
+'O python é uma linguagem de programação')
+criaCartao(
+'Geografia',
+'Qual a capital da França?',
+'A capital da França é Paris')
+criaCartao('Programaçao',
+    'O que é uma função',
+    'Uma funçao é um bloco de que codigo que executa alguma tarefa'
+)
