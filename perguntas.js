27daeda3fb0030948818df5d239e75dbba9f70b0
jvziin10 <joao.franca.santos22@escola.pr.gov.br>
2025-11-19 08:11:46 -0300

    j

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,23 @@
-criaCartao('Programação',
-'O que é python?',
-'O python é uma linguagem de programação')
 criaCartao(
-'Geografia',
-'Qual a capital da França?',
-'A capital da França é Paris')
-criaCartao('Programaçao',
-    'O que é uma função',
-    'Uma funçao é um bloco de que codigo que executa alguma tarefa'
-)criaCartao('lingua inglesa','Como se diz oi em inglês?','Oi em ingles é HI (RAI)')
-let respostaEstaVisivel = false
-function viraCartao() {
-    respostaEstaVisivel = !respostaEstaVisivel 
-    Cartao.classList.toggle('active',respostaEstaVisivel)}
-    cartao.addEventListener("click", viraCartao)
-container.appendChild(cartao)}
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
